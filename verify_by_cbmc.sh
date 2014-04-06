bfa08c0462b82e6b0e86fee9166b1afc466d9ba5
Ilya Levin <eli@literatecode.com>
2014-04-06 21:31:41 +0800

    Update verify_by_cbmc.sh
    removed unused "-I"

--- verify_by_cbmc.sh
+++ verify_by_cbmc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cbmc demo.c aes256.c -I./inc/ -I./arch/x86/ -D BACK_TO_TABLES --unwind 256 --bounds-check --pointer-check --refine
+cbmc demo.c aes256.c -D BACK_TO_TABLES --unwind 256 --bounds-check --pointer-check --refine
